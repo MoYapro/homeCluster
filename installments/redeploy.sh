e8f3715a43aeeaa6eddfb64a3eea5fdc76f52f12
ritmeier <tom.ritmeier@gmail.com>
2019-07-07 19:36:11 +0200

    delete all and apply again

---
+++ installments/redeploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+kubectl delete all --all -n kube-system
+echo "wait 10s for pods to terminate"
+sleep 10
+kubectl apply -f flannel/kube-flannel.yaml
+
+#kubectl apply -f traefik/traefik-rbac.yaml
+#kubectl apply -f traefik/traefik-daemonset.yaml
+
