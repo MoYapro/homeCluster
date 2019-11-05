750078bc9fb9f5af74f2542830a297df223ab451
ritmeier <tom.ritmeier@gmail.com>
2019-11-05 18:47:04 +0100

    add script to initialy setup shelly device

--- installments/setupShellyDevice.sh
+++ installments/setupShellyDevice.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-deviceIp='192.168.1.171'
+deviceIp='192.168.1.178'
 
 curl "http://${deviceIp}/settings?mqtt_enable=true&mqtt_server=192.168.1.101:31883&mqtt_user=homeassistant&mqtt_pass=password"
 
