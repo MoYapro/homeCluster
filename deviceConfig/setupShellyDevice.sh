9af30dedc8b2dcd66aa5d49b85dc7f32ab0fd9c4
Tom Ritmeier <tom.ritmeier@gmail.com>
2020-08-22 07:34:54 +0200

    prevent shelly in detached button mode to resend current state

--- deviceConfig/setupShellyDevice.sh
+++ deviceConfig/setupShellyDevice.sh
@@ -9,6 +9,6 @@ fi
 
 deviceIp=$1
 
-curl "http://${deviceIp}/settings?mqtt_enable=true&mqtt_server=192.168.1.101:31883&mqtt_user=homeassistant&mqtt_pass=password" | python -m json.tool
+curl "http://${deviceIp}/settings?mqtt_enable=true&mqtt_server=192.168.1.101:31883&mqtt_user=homeassistant&mqtt_pass=password&mqtt_update_period=0" | python -m json.tool
 
 
